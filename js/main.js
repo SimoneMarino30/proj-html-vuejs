10ae8c48dea48e0abdb67007df3c59f86e76f6f4
Simone Marino <simo.marino@outlook.it>
2023-02-20 17:35:50 +0100

    NavBar with Vue Scaffold

--- js/main.js
+++ js/main.js
@@ -1,8 +1,70 @@
+// ARRAY OGGETTI
+
 // VUE
 const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      navLinks: [
+        {
+          name: "Home",
+          dropdownMenu: false,
+        },
+
+        {
+          name: "Games",
+          dropdownMenu: true,
+          dropdowns: [
+            {
+              type: "Games",
+              action: "Games Details",
+              moreInfo: "Match Details",
+            },
+          ],
+        },
+
+        {
+          name: "Shop",
+          dropdownMenu: true,
+          dropdowns: [
+            {
+              type: "Shop",
+              action: "Weekly Offers",
+              moreInfo: "Further informations",
+            },
+          ],
+        },
+
+        {
+          name: "Pages",
+          dropdownMenu: true,
+          dropdowns: [
+            {
+              type: "Pages",
+              action: "Content",
+              moreInfo: "Further informations",
+            },
+          ],
+        },
+
+        {
+          name: "Blog",
+          dropdownMenu: true,
+          dropdowns: [
+            {
+              type: "Blog",
+              action: "Newsletter",
+              moreInfo: "Go to the blog",
+            },
+          ],
+        },
+
+        {
+          name: "Contact",
+          dropdownMenu: false,
+        },
+      ],
+    };
   },
 }).mount("#root");
